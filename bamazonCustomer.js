5b28b6081f2d02ba8d98ecd7aeb0600db4eb10b9
pperreiah <31325721+pperreiah@users.noreply.github.com>
2018-01-11 21:49:25 -0500

    final push for submission

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
-
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,55 +13,65 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    queryAllProducts();
+    queryAllProducts(getCustomerOrder);
+
 });
 
-function queryAllProducts() {
+function queryAllProducts(cb) {
     console.log("\n\n Displaying all products for your selection...\n");
     connection.query("SELECT * FROM products", function(err, res) {
 
         if (err) throw err;
+        console.table(res); //this creates the displayed table
 
-        // Log all results of the SELECT statement
-        console.log("ID" + "|" + " Product Name  " + "|" + " Department Name" + "|" + "Price");
-        console.log("--|--------------|-----------------|------");
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
-        }
-        console.log("------------------------------------------------\n\n");
+        // // Log all results of the SELECT statement  --but this fails to format the table correctly
+        // console.log("ID" + "|" + " Product Name  " + "|" + " Department Name" + "|" + "Price");
+        // console.log("--|--------------|-----------------|------");
+        // for (var i = 0; i < res.length; i++) {
+        //     console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+        // }
+        // console.log("------------------------------------------------\n\n");
 
-        //Get user product selection
-        inquirer
-            .prompt([{
-                    type: "input",
-                    message: "Type the ID number of the product you would like to purchase:",
-                    name: "item_id"
-                },
-                {
-                    type: "input",
-                    message: "How many units of this product would you like to purchase?",
-                    name: "quantity_request"
-                }
-            ])
-            .then(function(inquirerResponse) {
-                console.log("===>  Item selected = #" + inquirerResponse.item_id + "; Item Quantity Requested = " + inquirerResponse.quantity_request + " units.");
-                connection.query("SELECT stock_quantity, price FROM products WHERE item_id = ??", [inquirerResponse.item_id], function(err, res) {
+        cb(); //call getCustomerOrder()
 
-                    // if (err) throw err;
+    }); //connection query
+}; //function
 
-                    console.log("Item #" + inquirerResponse.item_id + " stock quantity = " + res.stock_quantity);
-                    if (res.stock_quantity < inquirerResponse.quantity_request) {
-                        console.log("Sorry, we do not have sufficient stock of Item #" + inquirerResponse.item_id + " to  fill your order.");
-                    } else {
-                        var newStockQty = res.stock_quantity - inquirerResponse.quantity_request;
-                        connection.query('UPDATE products SET stock_quantity = newStockQty WHERE item_id = inquirerResponse.item_id');
-                        console.log("Your order is being filled and will cost $" + res.price * inquirerResponse.quantity_request + ".  Thank you for your business.");
-                    } //else
 
-                }); //connection.query		  
-            }); //prompt -then
+//Get user product selection
+function getCustomerOrder() {
 
-        connection.end();
+    inquirer
+        .prompt([{
+                type: "input",
+                message: "Type the ID number of the product you would like to purchase:",
+                name: "item_id"
+            },
+            {
+                type: "input",
+                message: "How many units of this product would you like to purchase?",
+                name: "quantity_request"
+            }
+        ])
+        .then(function(inquirerResponse) {
+            //console.log("===>  Item selected = #" + inquirerResponse.item_id + "; Item Quantity Requested = " + inquirerResponse.quantity_request + " units.");
+            connection.query("SELECT stock_quantity, price FROM products WHERE item_id = ?", [inquirerResponse.item_id], function(err, results) {
 
-    }); //connection
-}; //function queryAllProducts 
+                // if (err) console.error(err);
+
+                console.log(results); //shows correct object is being returned
+                console.log("stock_quantity = " + results.stock_quantity) //shows results.stock_quantity is not accessing the value 
+                console.log("price = " + results.price) //shows results.price is not accessing the value 
+
+                if (results.stock_quantity < inquirerResponse.quantity_request) {
+                    console.log("Sorry, we do not have sufficient stock of Item #" + inquirerResponse.item_id + " to  fill your order.");
+                } else {
+                    var newStockQty = results.stock_quantity - inquirerResponse.quantity_request;
+                    connection.query('UPDATE products SET stock_quantity = newStockQty WHERE item_id = inquirerResponse.item_id');
+                    console.log("Your order is being filled and will cost $" + results.price * inquirerResponse.quantity_request + ".  Thank you for your business.");
+                } //else
+
+            }); //connection.query
+
+        }); // then
+}; //function getCustomerOrder
--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE products
 	item_id int NOT NULL AUTO_INCREMENT,
 	product_name varchar(255) NOT NULL,
 	department_name varchar(255) NOT NULL,
-	price int NOT NULL,
+	price decimal(15,2) NOT NULL,
 	stock_quantity int NOT NULL,
 	PRIMARY KEY (item_id)
 );
